1584bc549b3ac3aebc48dbe7bca14ef1dd6e6937
Jean-Philippe Fauconnier <jfauconnier@roblox.com>
2024-12-27 11:32:51 +0100

    FIX | Fix for nvim

--- python3/tiles/tiles.py
+++ python3/tiles/tiles.py
@@ -1,6 +1,5 @@
-import os
 import vim
-from typing import Dict
+from typing import Dict, Tuple
 
 WINDOW_CODE = "code"
 WINDOW_CONSOLE = "console"
@@ -10,39 +9,46 @@ WINDOW_STACK = "stack"
 WINDOW_VARIABLE = "variables"
 WINDOW_UNKNOWN = "unknown"
 
-class Tiles:
 
+class Tiles:
     def _append_dict(self, index: Dict, name: str, window_number: int):
         var = index.get(name, [])
         var.append(window_number)
         index[name] = var
         return index
 
-    def _equalize_code_windows(self):
-        name_to_numbers, numbers_to_name = self.index_window()
-        code_windows = name_to_numbers.get(WINDOW_CODE, [])
-
-
-    def index_window(self) -> Dict:
+    def index_window(self) -> Tuple:
         name_to_numbers = dict()
         numbers_to_name = dict()
 
         # Indexing Vimspector window
         for w in vim.windows:
-            if "Variables" in str(w.buffer):
-                name_to_numbers = self._append_dict(name_to_numbers, WINDOW_VARIABLE, w.number)
+            buf_name = str(vim.eval(f"bufname(winbufnr({w.number}))"))
+
+            if "Variables" in buf_name:
+                name_to_numbers = self._append_dict(
+                    name_to_numbers, WINDOW_VARIABLE, w.number
+                )
                 numbers_to_name[w.number] = WINDOW_VARIABLE
-            elif "StackTrace" in str(w.buffer):
-                name_to_numbers = self._append_dict(name_to_numbers, WINDOW_STACK, w.number)
+            elif "StackTrace" in buf_name:
+                name_to_numbers = self._append_dict(
+                    name_to_numbers, WINDOW_STACK, w.number
+                )
                 numbers_to_name[w.number] = WINDOW_STACK
-            elif "Console" in str(w.buffer):
-                name_to_numbers = self._append_dict(name_to_numbers, WINDOW_CONSOLE, w.number)
+            elif "Console" in buf_name:
+                name_to_numbers = self._append_dict(
+                    name_to_numbers, WINDOW_CONSOLE, w.number
+                )
                 numbers_to_name[w.number] = WINDOW_CONSOLE
-            elif "miniconda3" in str(w.buffer) or "bin/dlv" in str(w.buffer):
-                name_to_numbers = self._append_dict(name_to_numbers, WINDOW_OUTPUT, w.number)
+            elif "vimspector.Output" in buf_name:  # Updated this condition
+                name_to_numbers = self._append_dict(
+                    name_to_numbers, WINDOW_OUTPUT, w.number
+                )
                 numbers_to_name[w.number] = WINDOW_OUTPUT
-            elif "bin/zsh" in str(w.buffer):
-                name_to_numbers = self._append_dict(name_to_numbers, WINDOW_SHELL, w.number)
+            elif "term://" in buf_name:  # Updated terminal detection
+                name_to_numbers = self._append_dict(
+                    name_to_numbers, WINDOW_SHELL, w.number
+                )
                 numbers_to_name[w.number] = WINDOW_SHELL
 
         # Indexing code windows
@@ -50,7 +56,9 @@ class Tiles:
         output_number = name_to_numbers.get(WINDOW_OUTPUT, [0])[0]
         for w in vim.windows:
             if w.number > stack_number and w.number < output_number:
-                name_to_numbers = self._append_dict(name_to_numbers, WINDOW_CODE, w.number)
+                name_to_numbers = self._append_dict(
+                    name_to_numbers, WINDOW_CODE, w.number
+                )
                 numbers_to_name[w.number] = WINDOW_CODE
 
         return name_to_numbers, numbers_to_name
@@ -73,12 +81,14 @@ class Tiles:
             print("Nothing to re-tiles")
             return None
 
-        name_to_numbers, numbers_to_name = self.index_window()
+        _, numbers_to_name = self.index_window()
         number_of_lines = int(vim.eval("&lines"))
         if number_of_lines < 60:
             # Small display
             for w in vim.windows:
-                name = numbers_to_name.get(w.number, WINDOW_UNKNOWN + "_" + str(w.buffer))
+                name = numbers_to_name.get(
+                    w.number, WINDOW_UNKNOWN + "_" + str(w.buffer)
+                )
                 if name is WINDOW_VARIABLE:
                     w.height = 34
                     w.width = 40
@@ -97,7 +107,9 @@ class Tiles:
         else:
             # larger display
             for w in vim.windows:
-                name = numbers_to_name.get(w.number, WINDOW_UNKNOWN + "_" + str(w.buffer))
+                name = numbers_to_name.get(
+                    w.number, WINDOW_UNKNOWN + "_" + str(w.buffer)
+                )
                 if name is WINDOW_VARIABLE:
                     w.height = 60
                     w.width = 75
@@ -119,12 +131,14 @@ class Tiles:
             print("Nothing to re-tiles")
             return None
 
-        name_to_numbers, numbers_to_name = self.index_window()
+        _, numbers_to_name = self.index_window()
         number_of_lines = int(vim.eval("&lines"))
         if number_of_lines < 60:
             # Small display
             for w in vim.windows:
-                name = numbers_to_name.get(w.number, WINDOW_UNKNOWN + "_" + str(w.buffer))
+                name = numbers_to_name.get(
+                    w.number, WINDOW_UNKNOWN + "_" + str(w.buffer)
+                )
                 if name is WINDOW_VARIABLE:
                     w.height = 38
                     w.width = 32
@@ -143,7 +157,9 @@ class Tiles:
         else:
             # larger display
             for w in vim.windows:
-                name = numbers_to_name.get(w.number, WINDOW_UNKNOWN + "_" + str(w.buffer))
+                name = numbers_to_name.get(
+                    w.number, WINDOW_UNKNOWN + "_" + str(w.buffer)
+                )
                 if name is WINDOW_VARIABLE:
                     w.height = 59
                     w.width = 35
@@ -165,12 +181,14 @@ class Tiles:
             print("Nothing to re-tiles")
             return None
 
-        name_to_numbers, numbers_to_name = self.index_window()
+        _, numbers_to_name = self.index_window()
         number_of_lines = int(vim.eval("&lines"))
         if number_of_lines < 60:
             # Small display
             for w in vim.windows:
-                name = numbers_to_name.get(w.number, WINDOW_UNKNOWN + "_" + str(w.buffer))
+                name = numbers_to_name.get(
+                    w.number, WINDOW_UNKNOWN + "_" + str(w.buffer)
+                )
                 if name is WINDOW_VARIABLE:
                     w.height = 40
                     w.width = 12
@@ -189,7 +207,9 @@ class Tiles:
         else:
             # Larger display
             for w in vim.windows:
-                name = numbers_to_name.get(w.number, WINDOW_UNKNOWN + "_" + str(w.buffer))
+                name = numbers_to_name.get(
+                    w.number, WINDOW_UNKNOWN + "_" + str(w.buffer)
+                )
                 if name is WINDOW_VARIABLE:
                     w.height = 67
                     w.width = 19
@@ -211,12 +231,14 @@ class Tiles:
             print("Nothing to re-tiles")
             return None
 
-        name_to_numbers, numbers_to_name = self.index_window()
+        _, numbers_to_name = self.index_window()
         number_of_lines = int(vim.eval("&lines"))
         if number_of_lines < 60:
             # Small display
             for w in vim.windows:
-                name = numbers_to_name.get(w.number, WINDOW_UNKNOWN + "_" + str(w.buffer))
+                name = numbers_to_name.get(
+                    w.number, WINDOW_UNKNOWN + "_" + str(w.buffer)
+                )
                 if name is WINDOW_VARIABLE:
                     w.height = 34
                     w.width = 67
@@ -235,7 +257,9 @@ class Tiles:
         else:
             # Larger display
             for w in vim.windows:
-                name = numbers_to_name.get(w.number, WINDOW_UNKNOWN + "_" + str(w.buffer))
+                name = numbers_to_name.get(
+                    w.number, WINDOW_UNKNOWN + "_" + str(w.buffer)
+                )
                 if name is WINDOW_VARIABLE:
                     w.height = 59
                     w.width = 94
@@ -252,18 +276,19 @@ class Tiles:
                     w.height = 15
                     w.width = 125
 
-
     def vimspector_stack_focus(self) -> None:
         if len(vim.windows) < 5:
             print("Nothing to re-tiles")
             return None
 
-        name_to_numbers, numbers_to_name = self.index_window()
+        _, numbers_to_name = self.index_window()
         number_of_lines = int(vim.eval("&lines"))
         if number_of_lines < 60:
             # Small display
             for w in vim.windows:
-                name = numbers_to_name.get(w.number, WINDOW_UNKNOWN + "_" + str(w.buffer))
+                name = numbers_to_name.get(
+                    w.number, WINDOW_UNKNOWN + "_" + str(w.buffer)
+                )
                 if name is WINDOW_VARIABLE:
                     w.height = 6
                     w.width = 67
@@ -282,7 +307,9 @@ class Tiles:
         else:
             # Larger display
             for w in vim.windows:
-                name = numbers_to_name.get(w.number, WINDOW_UNKNOWN + "_" + str(w.buffer))
+                name = numbers_to_name.get(
+                    w.number, WINDOW_UNKNOWN + "_" + str(w.buffer)
+                )
                 if name is WINDOW_VARIABLE:
                     w.height = 19
                     w.width = 94
